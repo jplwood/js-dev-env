ba3dae9e79d2bd934a678588f82844993003671a
Justin Leatherwood <jplwood@gmail.com>
2016-11-22 22:36:03 -0600

    added babel transpiling

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,7 @@
-var chalk = require('chalk'); // allows you to specify color displayed to console!
+/* allows you to specify color displayed to console!
+ *    using es2015 module syntax instead of
+ *    CommonJS node style: var a = require("a"); */
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'))
 
